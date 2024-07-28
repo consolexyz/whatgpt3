65ee8c94949b5f0e892a97e517a9a638240c365f
consolexyz <mosesobike@gmail.com>
2024-07-28 05:51:49 +0100

    added footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,3 +1,6 @@
+import gptLogo from "../../assets/logo.svg"
+import "./footer.css"
+
 function Footer() {
     return (<div className="gpt3_footer  section__padding">
         <div className="gpt3_footer_heading">
@@ -10,7 +13,36 @@ function Footer() {
         <div className="gpt3_footer_links">
             <div className="gpt3_footer_links_logo">
                 <img src={gptLogo} alt="logo" />
+                <p> Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
             </div>
+
+            <div className="gpt3_footer_links_div">
+                <h4>Links</h4>
+                <p>Overons</p>
+                <p>Social Media</p>
+                <p>Counters</p>
+                <p>Contacts</p>
+            </div>
+
+            <div className="gpt3_footer_links_div">
+                <h4>Company</h4>
+                <p>Terms & Conditions</p>
+                <p>Privacy Policy</p>
+                <p>Contact</p>
+            </div>
+
+            <div className="gpt3_footer_links_div">
+                <h4>Get in touch</h4>
+                <p>Crechterwoord K12 182 DK Alknjkcb</p>
+                <p>085-132567</p>
+                <p>info@payme.net</p>
+            </div>
+
+
+        </div>
+
+        <div className="gpt3_footer_copyright">
+            <p>© 2021 GPT-3. All rights reserved.</p>
         </div>
     </div>);
 }
